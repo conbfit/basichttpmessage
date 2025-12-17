1feb2862b5baa2d9d5c44d91200d7b2f5597c0e2
conbfit <conbfit@pm.me>
2025-12-17 17:34:44 -0600

    first scripts

---
+++ socket_client.py
@@ -0,0 +1,42 @@
+import socket
+import sys
+
+def send_message(host,port,message):
+
+    # create an ip socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # connect to the server
+        print(f"\n[*] connecting to {host}:{port}")
+        client_socket.connect((host,port))
+        print(f"\n[+] connected")
+
+        # send the message
+        print(f"\n[*] sending: {message}")
+        client_socket.sendall(message.encode('utf-8'))
+
+        # receive response
+        response = client_socket.recv(1024)
+        print(f"[Response] {response.decode('utf-8')}")
+
+    except ConnectionRefusedError:
+        print(f"\n[!] connection refused")
+        sys.exit(1)
+    except OSError as e:
+        print(f"\n[!] network error: {e}")
+        sys.exit(1)
+    finally:
+        client_socket.close()
+        print("\n[*] connection closed")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("incorrect usage")
+        sys.exit(1)
+
+    message = sys.argv[1]
+    host = sys.argv[2] if len(sys.argv)>2 else "localhost"
+    port = int(sys.argv[3]) if len(sys.argv)>3 else 5555
+
+    send_message(host,port, message)
---
+++ socket_server.py
@@ -0,0 +1,47 @@
+import socket
+import sys
+
+def start_server(host='0.0.0.0',port=5555):
+
+    #create socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # allow address reuse
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # bind socket to port
+    try:
+        server_socket.bind((host, port))
+        print(f"[*] server bound to {host}:{port}")
+    except OSError as e:
+        print(f"[!] failed to bind to {host}:{port}: {e}")
+        sys.exit(1)
+
+    try:
+        while True:
+            #accept connection
+            client_socket, client_address = server_socket.accept()
+            print(f"\n[+] connection from {client_address[0]}:{client_address[1]}")
+
+            #receive data
+            data = client_socket.recv(1024)
+
+            if data:
+                message = data.decode('utf-8')
+                print(f"[Message] {message}")
+            else:
+                printf("[*] disconnected with no data sent")
+
+            response = "Message received!\n"
+            client_socket.sendall(response.encode('utf-8'))
+
+            #close server
+            client_socket.close()
+
+    except KeyboardInterrupt:
+        print("\n[*] closing server")
+    finally:
+        server_socket.close()
+
+    if __name__ == '__main__':
+        start_server()
