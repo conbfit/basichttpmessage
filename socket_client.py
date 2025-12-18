398181c5902a56d50c2d9f5c1bc0d9439cb41195
conbfit <conbfit@pm.me>
2025-12-17 18:40:17 -0600

    nvm NOW it works

--- socket_client.py
+++ socket_client.py
@@ -31,6 +31,7 @@ def send_message(host,port,message):
         print("\n[*] connection closed")
 
 if __name__ == "__main__":
+    print(f"sys.argv={sys.argv}")
     if len(sys.argv) < 2:
         print("incorrect usage")
         sys.exit(1)
